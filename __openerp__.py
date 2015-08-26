901a1a05de889f0247ef7b9a80f477a9ff2d135e
Jarmo Kortetjärvi <jarmo@futurable.fi>
2015-08-26 15:08:43 +0300

    Initial commit

---
+++ __openerp__.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#   Copyright (c) 2015- Oy Tawasta Technologies Ltd. (http://www.tawasta.fi)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    'name': 'Web UI Alternative',
+    'category': 'Web',
+    'version': '0.1',
+    'author': 'Oy Tawasta Technologies Ltd',
+    'website': 'http://www.tawasta.fi',
+    'depends': [
+        'web',
+    ],
+    'description': '''
+Web UI Alternative
+==================
+
+Alternative appearance for the web UI
+''',
+    'data': [
+        'view/web_ui_alternative.xml'
+    ],
+}
