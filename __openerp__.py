dcdddac01d4b0b14318a994fcd38ba327aef358f
Jarmo Kortetjärvi <jarmo@futurable.fi>
2015-10-15 08:58:26 +0300

    Reverse the row coloring order

--- __openerp__.py
+++ __openerp__.py
@@ -15,7 +15,7 @@
 {
     'name': 'Web UI Alternative',
     'category': 'Web',
-    'version': '8.0.0.1.1',
+    'version': '8.0.0.1.2',
     'author': 'Oy Tawasta Technologies Ltd',
     'website': 'http://www.tawasta.fi',
     'depends': [
