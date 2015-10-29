912030d83d8ad744abfbf1dc595a23dbfaf865f7
Jarmo Kortetjärvi <jarmo@futurable.fi>
2015-10-29 16:21:18 +0200

    Improve toggle buttons

--- __openerp__.py
+++ __openerp__.py
@@ -15,7 +15,7 @@
 {
     'name': 'Web UI Alternative',
     'category': 'Web',
-    'version': '8.0.0.1.4',
+    'version': '8.0.0.1.5',
     'author': 'Oy Tawasta Technologies Ltd',
     'website': 'http://www.tawasta.fi',
     'depends': [
