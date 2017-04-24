8cdb8ce3e15523dc1aa1476c42cf610481a5f567
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2017-04-24 14:21:10 +0300

    Fix the fetched font url

--- web_ui_alternative/__openerp__.py
+++ web_ui_alternative/__openerp__.py
@@ -21,7 +21,7 @@
 {
     'name': 'Web UI Alternative',
     'category': 'Web',
-    'version': '8.0.0.1.12',
+    'version': '8.0.0.1.13',
     'author': 'Oy Tawasta Technologies Ltd',
     'website': 'http://www.tawasta.fi',
     'depends': [
