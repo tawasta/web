d9f3e249a2000790ff692cef122d8aca6bb936d9
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2017-11-09 11:10:51 +0200

    Improve Firefox compatibility

--- web_ui_alternative/__openerp__.py
+++ web_ui_alternative/__openerp__.py
@@ -21,7 +21,7 @@
 {
     'name': 'Web UI Alternative',
     'category': 'Web',
-    'version': '8.0.0.1.15',
+    'version': '8.0.0.1.16',
     'author': 'Oy Tawasta Technologies Ltd',
     'website': 'http://www.tawasta.fi',
     'depends': [
