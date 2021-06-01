7e1844a3bf47c98e66c40f6e953b600e261d0102
difbit <timo.kekalainen@hotmail.com>
2021-06-01 09:30:07 +0300

    [MIG] Migration to 14.0

---
+++ web_report_sfs/__manifest__.py
@@ -0,0 +1,37 @@
+##############################################################################
+#
+#    Author: Oy Tawasta OS Technologies Ltd.
+#    Copyright 2020 Oy Tawasta OS Technologies Ltd. (https://tawasta.fi)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program. If not, see http://www.gnu.org/licenses/agpl.html
+#
+##############################################################################
+
+{
+    "name": "SFS 2487 report layout",
+    "summary": "Alter report layout to follow SFS 2487 standard formatting",
+    "version": "14.0.1.0.0",
+    "category": "Web",
+    "website": "https://github.com/Tawasta/web",
+    "author": "Tawasta",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": ["web"],
+    "data": [
+        "data/report_paperformat.xml",
+        "data/company_paperformat.xml",
+        "views/report_templates.xml",
+    ],
+}
