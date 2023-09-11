21fb3c23db23abf7fada4db1f471425da72f6795
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2023-09-11 14:54:20 +0300

    [REM] Broken reference to a non-existent Google font "Arial"

--- web_arial_font/__manifest__.py
+++ web_arial_font/__manifest__.py
@@ -21,7 +21,7 @@
 {
     "name": "Arial font option for PDF prints",
     "summary": "Arial font option for PDF prints",
-    "version": "14.0.1.0.0",
+    "version": "14.0.1.0.1",
     "category": "Web",
     "website": "https://gitlab.com/tawasta/odoo/web",
     "author": "Tawasta",
@@ -31,5 +31,5 @@
     "depends": [
         "web",
     ],
-    "data": ["views/report_assets_common.xml"],
+    "data": [],
 }
