a9e4c164d555c77cd6d5895f6f47c642f59b9d26
Valtteri Lattu <valtteri.lattu@tawasta.fi>
2026-01-20 07:44:33 +0200

    [IMP] - fixed function

--- web_content_group_permissions/controllers/main.py
+++ web_content_group_permissions/controllers/main.py
@@ -1,106 +1,53 @@
-##############################################################################
-#
-#    Author: Futural Oy
-#    Copyright 2021- Futural Oy (https://futural.fi)
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program. If not, see http://www.gnu.org/licenses/agpl.html
-#
-##############################################################################
-
-# 1. Standard library imports:
 import logging
-
-# 3. Odoo imports (openerp):
 from odoo import _, http
 from odoo.exceptions import AccessDenied
 from odoo.http import request
-
-# 4. Imports from Odoo modules:
 from odoo.addons.web.controllers.binary import Binary
-
-# 2. Known third party imports:
-
-
-# 5. Local imports in the relative form:
-
-# 6. Unknown third party imports:
 
 _logger = logging.getLogger(__name__)
 
-
 class BinaryGroupPermissions(Binary):
-    # disable pylint error as id definition is needed in inheritance
-    # pylint: disable=W0622
-    @http.route(
-        [
-            "/web/content",
-            "/web/content/<string:xmlid>",
-            "/web/content/<string:xmlid>/<string:filename>",
-            "/web/content/<int:id>",
-            "/web/content/<int:id>/<string:filename>",
-            "/web/content/<int:id>-<string:unique>",
-            "/web/content/<int:id>-<string:unique>/<string:filename>",
-            "/web/content/<int:id>-<string:unique>/<path:extra>/<string:filename>",
-            "/web/content/<string:model>/<int:id>/<string:field>",
-            "/web/content/<string:model>/<int:id>/<string:field>/<string:filename>",
-        ],
-        type="http",
-        auth="public",
-    )
-    def content_common(
-        self,
-        xmlid=None,
-        model="ir.attachment",
-        id=None,
-        field="datas",
-        filename=None,
-        filename_field="name",
-        unique=None,
-        mimetype=None,
-        download=None,
-        data=None,
-        token=None,
-        access_token=None,
-        **kw,
-    ):
-        attachment_id = request.env["ir.attachment"].sudo().search([["id", "=", id]])
-        allowed_groups = attachment_id.visibility_group_ids
-        user_id = request.env.user
-        if allowed_groups and all(
-            user_id not in allowed_group.users for allowed_group in allowed_groups
-        ):
-            _logger.info(
-                "Access Denied to ir.attachment record %s for user %s",
-                attachment_id.name,
-                user_id.name,
-            )
-            raise AccessDenied(
-                _("You don't have permission to access the requested resource.")
-            )
 
+    @http.route([
+        '/web/content',
+        '/web/content/<string:xmlid>',
+        '/web/content/<string:xmlid>/<string:filename>',
+        '/web/content/<int:id>',
+        '/web/content/<int:id>/<string:filename>',
+        '/web/content/<string:model>/<int:id>/<string:field>',
+        '/web/content/<string:model>/<int:id>/<string:field>/<string:filename>',
+    ], type='http', auth="public")
+    def content_common(self, xmlid=None, model='ir.attachment', id=None, field='raw',
+                       filename=None, filename_field='name', mimetype=None, unique=False,
+                       download=False, access_token=None, nocache=False, **kw):
+
+        # Vain jos kyseessä on nimenomaan ir.attachment ja id on annettu
+        if model == 'ir.attachment' and id:
+            attachment = request.env['ir.attachment'].sudo().browse(int(id))
+            if attachment.exists():
+                allowed_groups = attachment.visibility_group_ids
+                user = request.env.user
+
+                # Jos ryhmiä on asetettu, käyttäjän pitää kuulua vähintään yhteen niistä
+                if allowed_groups and not (allowed_groups & user.groups_id):
+                    _logger.info(
+                        "Access Denied to ir.attachment record %s for user %s",
+                        attachment.name, user.name
+                    )
+                    raise AccessDenied(_("You don't have permission to access the requested resource."))
+
+        # Kutsu parenttia Odoo 17:n parametreilla
         return super().content_common(
-            xmlid,
-            model,
-            id,
-            field,
-            filename,
-            filename_field,
-            unique,
-            mimetype,
-            download,
-            data,
-            token,
-            access_token,
-            **kw,
+            xmlid=xmlid,
+            model=model,
+            id=id,
+            field=field,
+            filename=filename,
+            filename_field=filename_field,
+            mimetype=mimetype,
+            unique=unique,
+            download=download,
+            access_token=access_token,
+            nocache=nocache,
+            **kw
         )
