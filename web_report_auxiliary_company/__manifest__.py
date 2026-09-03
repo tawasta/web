f4c39f6d18aadad482e0367299bd2866cb005406
difbit <timo.kekalainen@tawasta.fi>
2026-09-03 11:27:31 +0300

    [IMP] web_report_auxiliary_company module: Added is_aux_company field to company to be used in domain

--- web_report_auxiliary_company/__manifest__.py
+++ web_report_auxiliary_company/__manifest__.py
@@ -21,7 +21,7 @@
 {
     "name": "Web report – Auxiliary company",
     "summary": "Auxiliary company used in PDF prints",
-    "version": "19.0.1.0.1",
+    "version": "19.0.1.0.2",
     "category": "Reporting",
     "website": "https://github.com/tawasta/web",
     "author": "Futural",
@@ -38,6 +38,7 @@
     "data": [
         "views/account_move.xml",
         "views/purchase_order.xml",
+        "views/res_company.xml",
         "views/sale_order.xml",
         "views/stock_picking.xml",
         "report/invoice_report.xml",
